b5e7082ff6cff81d8cd22aad02d13accefd834b0
aglickman <aglickman305@Ufl.edu>
2019-03-12 13:29:43 -0400

    Planning dashboard on same page

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,4 +1,4 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     /* Get all the listings, then bind it to the scope */
     Listings.getAll().then(function(response) {
@@ -10,8 +10,8 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
 
     $scope.addListing = function() {
-	  /**TODO 
-	  *Save the article using the Listings factory. If the object is successfully 
+	  /**TODO
+	  *Save the article using the Listings factory. If the object is successfully
     saved redirect back to the list page. Otherwise, display the error
 	 */
       var listing = {
@@ -29,8 +29,8 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     $scope.deleteListing = function(id) {
 	   /**TODO
-        Delete the article using the Listings factory. If the removal is successful, 
-		navigate back to 'listing.list'. Otherwise, display the error. 
+        Delete the article using the Listings factory. If the removal is successful,
+		navigate back to 'listing.list'. Otherwise, display the error.
        */
       Listings.delete(id);
       Listings.getAll().then(function(response) {
@@ -45,3 +45,10 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
   }
 ]);
+
+function myFunction() {
+  var divsToHide = document.getElementsByClassName(".lowin");
+  for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility="hidden";
+  }
+}
