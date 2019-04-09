5881c077d3879ebae2bd717fc6a27c1d28f5d3fd
Ziyang Chen <aa314580571@ufl.edu>
2019-04-10 01:32:43 +0800

    merge conflic resolved

--- client/js/controllers/tweetsController.js
+++ client/js/controllers/tweetsController.js
@@ -6,15 +6,11 @@ app.controller('TweetsController', ['$scope', '$rootScope','TweetsFactory',
     $scope.showTweets = function() {
       TweetsFactory.create($rootScope.trendQuery).then(
         function(response) {
-<<<<<<< HEAD
           var tweets = [];
           for(var i=0; i<5; i++){
             tweets.push(response.data[i]);
           }
           $scope.tweetsArray=tweets;
-=======
-          $scope.tweets = response;
->>>>>>> 73d59ee37779563c6ef03e466d47c543f575928e
         },
         function(error) {
           console.log('Unable to retrieve tweets:', error);
