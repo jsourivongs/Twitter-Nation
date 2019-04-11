50503e791a1c8921836a0757d479ab556c8e8446
Ziyang Chen <aa314580571@ufl.edu>
2019-04-12 07:09:00 +0800

    fixing

--- client/js/controllers/tweetsController.js
+++ client/js/controllers/tweetsController.js
@@ -2,7 +2,7 @@ app.controller('TweetsController', ['$scope', '$rootScope','TweetsFactory',
   function ($scope, $rootScope, TweetsFactory) {
 
     $scope.tweetsArray;
-
+    const tweets = document.getElementById("tweets");
     $scope.showTweets = function() {
       TweetsFactory.create($rootScope.trendQuery).then(
         function(response) {
@@ -11,12 +11,21 @@ app.controller('TweetsController', ['$scope', '$rootScope','TweetsFactory',
             tweets.push(response.data[i]);
           }
           $scope.tweetsArray=tweets;
+          console.log("done");
         },
         function(error) {
           console.log('Unable to retrieve tweets:', error);
         }
       )
 		}
+
+    $scope.stringBasedHTML = function(){
+      var s = $scope.tweetsArray[0];
+      var index = s.indexOf("<script");
+      s=s.substring(0,index);
+      console.log(s);
+      tweets.innerHTML = s;
+    }
 
 		// Show tweet data initially
 		// $scope.showTweets();
