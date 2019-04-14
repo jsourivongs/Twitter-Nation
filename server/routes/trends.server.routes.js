e258816d9e6d8a26821732d6efc7b0e9d132be33
Sam Mercado <samueljmercado@yahoo.com>
2019-04-14 16:14:43 -0400

    Change trends requests to GET instead of POST

--- server/routes/trends.server.routes.js
+++ server/routes/trends.server.routes.js
@@ -3,11 +3,7 @@ var trends = require('../controllers/trends.server.controller.js'),
     express = require('express'),
     router = express.Router();
 
-// router.route('/')
-//   .get(trends.list)
-//   .post(trends.create);
-
 router.route('/:stateCode')
-  .post(trends.create);
+  .get(trends.list);
 
 module.exports = router;
