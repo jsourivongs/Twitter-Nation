24fd4a8ae79ba81dc106009645e6479443635e31
Julian Sourivongs <jsourivongs@gmail.com>
2019-04-24 23:04:54 -0400

    updated heroku urls

--- client/js/factories/tweetsFactory.js
+++ client/js/factories/tweetsFactory.js
@@ -1,10 +1,10 @@
 app.factory('TweetsFactory', function($http) {
     var methods = {
       getTweetsByQuery: function(searchQuery) {
-        return $http.get('https://pacific-chamber-75209.herokuapp.com/api/tweets/' + searchQuery);
+        return $http.get('https://twitter-nation.herokuapp.com/api/tweets/' + searchQuery);
       },
       create: function(trends){
-        return $http.post('https://pacific-chamber-75209.herokuapp.com/api/tweets', trends);
+        return $http.post('https://twitter-nation.herokuapp.com/api/tweets', trends);
       }
     }
 
